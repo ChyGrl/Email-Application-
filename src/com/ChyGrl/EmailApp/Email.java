d69219db6fb6a3be03b8ecb7094f317a02f62f1e
ChyGrl <32235891+ChyGrl@users.noreply.github.com>
2017-12-01 18:02:44 -0500

    4
    Updating Email apps

--- src/com/ChyGrl/EmailApp/Email.java
+++ src/com/ChyGrl/EmailApp/Email.java
@@ -1,28 +1,51 @@
 package com.ChyGrl.EmailApp;
 
+import java.util.Scanner;
+
 public class Email {
-	//this is private so that it is not access directly
-	private String firstname;
+	// this is private so that it is not access directly
+	private String firstName;
 	private String lastName;
 	private String password;
 	private String department;
 	private int mailboxCapacity;
 	private String alternateEmail;
-	
-	
-	
-	//Constructor to recieve first and last name
-	
-	
-	
-	//Ask for department
-	
-	
-	//Generate a random password
-	
-	
-	//Set mailbox capacity
-	
-	
-	//set the alternate email
+
+	// Constructor to receive first and last name
+
+	public Email(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		System.out.println("EMAIL CREATED:" + " " + this.firstName + " " + this.lastName);
+		// call a method asking for department and return department
+		this.department = setDepartment();
+	}
+
+	// Ask for department
+	private String setDepartment() {
+		System.out.print("Enter the Department\n 1 for Sales\n 2 for Development \n 3 for Accounting\n 0 for NONE");
+		Scanner in = new Scanner(System.in);
+
+		int depChoice = in.nextInt();
+
+		if (depChoice == 1) {
+			return "Sales";
+
+		} else if (depChoice == 2) {
+			return "Development";
+
+		} else if (depChoice == 3) {
+			return "Accounting";
+
+		} else {
+			return "";
+		}
+
+	}
+
+	// Generate a random password
+
+	// Set mailbox capacity
+
+	// set the alternate email
 }
--- src/com/ChyGrl/EmailApp/EmailApp.java
+++ src/com/ChyGrl/EmailApp/EmailApp.java
@@ -2,7 +2,7 @@ package com.ChyGrl.EmailApp;
 
 public class EmailApp {
 	public static void main(String[] args) {
-
+		Email email = new Email("Joe","Doe");
 		
 	}
 
