6f5cfbd1d7512a600f7ec59d5adac2528b43aaab
ChyGrl <32235891+ChyGrl@users.noreply.github.com>
2017-12-01 17:36:10 -0500

    Created Outline
    Basic outline for EmailApp

---
+++ src/com/ChyGrl/EmailApp/Email.java
@@ -0,0 +1,5 @@
+package com.ChyGrl.EmailApp;
+
+public class Email {
+
+}
---
+++ src/com/ChyGrl/EmailApp/EmailApp.java
@@ -0,0 +1,10 @@
+package com.ChyGrl.EmailApp;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
