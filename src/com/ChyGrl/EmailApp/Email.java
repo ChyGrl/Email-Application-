1b24df2f8028fdfe7c0cdb64c97630d402d6d5d1
ChyGrl <32235891+ChyGrl@users.noreply.github.com>
2017-12-01 22:52:47 -0500

    Commiting again

--- src/com/ChyGrl/EmailApp/Email.java
+++ src/com/ChyGrl/EmailApp/Email.java
@@ -17,13 +17,27 @@ public class Email {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		System.out.println("EMAIL CREATED:" + " " + this.firstName + " " + this.lastName);
+
 		// call a method asking for department and return department
 		this.department = setDepartment();
+		System.out.println("Department:" + this.department);
 	}
+	
+	//Generate a random password
+	private String  ramdomPassword(int length) {
+		String passwordSet= "ABCDEFGHIJKLMNOPQRSTUVWYZ0123456789!@#$%";
+		char [] password= new char[length];
+		for(int i = 0; i<length; i++) {
+			int rand = (int)(Math.random() * passwordSet.length());
+			password[i] =passwordSet.charAt(rand);
+		}
+	}
+	
+	
 
 	// Ask for department
 	private String setDepartment() {
-		System.out.print("Enter the Department\n 1 for Sales\n 2 for Development \n 3 for Accounting\n 0 for NONE");
+		System.out.print("DEPARTMENT CODES\n 1 for Sales\n 2 for Development \n 3 for Accounting\n 0 for NONE\n Enter Department Code");
 		Scanner in = new Scanner(System.in);
 
 		int depChoice = in.nextInt();
--- src/com/ChyGrl/EmailApp/EmailApp.java
+++ src/com/ChyGrl/EmailApp/EmailApp.java
@@ -3,7 +3,7 @@ package com.ChyGrl.EmailApp;
 public class EmailApp {
 	public static void main(String[] args) {
 		Email email = new Email("Joe","Doe");
-		
+		 
 	}
 
 }
